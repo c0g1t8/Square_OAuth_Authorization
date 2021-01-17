3a434ad1aff56ea552b6473a5a6309ff80ff8ce3
Gary Chan <gary.chan@c0g1t8.com>
2021-01-17 09:42:07 -0800

    minimal console with kestrel

---
+++ Authorize/Controllers/SayHi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorize.Controllers
+{
+    public class SayHi : ControllerBase
+    {
+        [Route("sayhi/{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
---
+++ Authorize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Authorize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+               .UseKestrel()
+               .UseUrls("http://*:5000")
+               .UseStartup<Startup>()
+               .Build();
+
+            host.Run();
+        }
+    }
+}
