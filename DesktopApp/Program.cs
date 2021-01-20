c55669a6c24ff2926fc23f64e7e96e22d6ee9dc4
Gary Chan <gary.chan@c0g1t8.com>
2021-01-19 16:20:48 -0800

    create solution and projects

---
+++ DesktopApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesktopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
