f3cbd1a552882e7480c6ac6aa13061a33e342d02
Gary Chan <gary.chan@c0g1t8.com>
2021-01-24 11:04:29 -0800

    adds response api calls

---
+++ AuthorizationProxy/Controllers/AuthorizationResponse.cs
@@ -0,0 +1,33 @@
+﻿using AuthorizationProxy.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationProxy.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthorizationResponse : ControllerBase
+    {
+        private readonly AuthorizationResponseService responseService;
+        private readonly ILogger<AuthorizationResponse> log;
+
+        public AuthorizationResponse(
+            AuthorizationResponseService responseService,
+            ILogger<AuthorizationResponse> log)
+        {
+            this.responseService = responseService;
+            this.log = log;
+        }
+
+        [HttpGet]
+        public IActionResult GetSquareRespoonse()
+        {
+            return Ok(responseService.AuthorizationResponse);
+        }
+    }
+}
---
+++ AuthorizationProxy/Controllers/OAuth-Redirect.cs
@@ -0,0 +1,41 @@
+﻿using AuthorizationProxy.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AuthorizationProxy.Controllers
+{
+    [Route("oauth-redirect")]
+    [ApiController]
+    public class OAuth_Redirect : ControllerBase
+    {
+        private readonly AuthorizationResponseService responseService;
+        private readonly ILogger<OAuth_Redirect> log;
+
+        public OAuth_Redirect(
+            AuthorizationResponseService responseService,
+            ILogger<OAuth_Redirect> log)
+        {
+            this.responseService = responseService;
+            this.log = log;
+        }
+
+        [HttpGet]
+        public IActionResult GetSquareResponse()
+        {
+            var queryString = HttpContext.Request.QueryString.Value;
+            var dict = HttpUtility.ParseQueryString(queryString);
+            var json = JsonSerializer.Serialize(dict.AllKeys.ToDictionary(k => k, k => dict[k]));
+
+            responseService.AuthorizationResponse = json;
+
+            return Ok("Authorization process completed.");
+        }
+    }
+}
